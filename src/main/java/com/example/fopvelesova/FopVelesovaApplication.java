708b3e428787f6d2b27a8e05e9f842d8546051a4
frold <frolovdanilik@gmail.com>
2022-11-25 12:03:13 +0200

    first commit

---
+++ src/main/java/com/example/fopvelesova/FopVelesovaApplication.java
@@ -0,0 +1,13 @@
+package com.example.fopvelesova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FopVelesovaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FopVelesovaApplication.class, args);
+    }
+
+}
